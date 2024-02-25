09a61ad79adc1875016cf16bb1c4944380231eed
Moises Corbellari <moisescorbellari1@outlook.com>
2024-02-25 13:42:50 -0300

    study

--- HangmanGame/HangmanGame.py
+++ HangmanGame/HangmanGame.py
@@ -1,10 +1,9 @@
+from re import T
 from time import sleep
 from random import choice
-
+import os
 def limpar_tela():
-    import os
     os.system('cls' if os.name == 'nt' else 'clear')
-
 instrucoes = '''      Jogo da Forca!
 
 Instruções:
@@ -19,18 +18,15 @@ Instruções:
 print(instrucoes)
 sleep(15)
 limpar_tela()
-
 usuario = input('Escolha seu nick: ').strip().title()
 print('.......... Bem-vindo ao Jogo da Forca, {}! ..........'.format(usuario))
-input('Pressione ENTER para jogar!')
-
-def contagem_regressiva(segundos):
-    for t in range(segundos, 0, -1):
+print('Pressione ENTER para jogar!', end='')
+input()
+def contagem_regressiva(t):
+    for t in range(t, 0, -1):
         print('Iniciando em {}'.format(t))
         sleep(1)
-
 contagem_regressiva(3)
-
 print('''Escolha o tema:
 
         [1] Fruta
@@ -38,74 +34,68 @@ print('''Escolha o tema:
         [3] Humano
         [4] Casa
     ''')
-
-escolha = int(input('Opção: '))
-
+while True:
+    try:
+        escolha = int(input('Opção: '))
+        if escolha<1 or escolha>4:
+            raise ValueError
+        break
+    except ValueError:
+        print('Opção inválida! Tente novamente.')
 if escolha == 1:
     tema = 'Fruta'
-    palavra = ['Banana', 'Abacaxi', 'Melao', 'Maça', 'Morango', 'Laranja', 'Mamao', 
-               'Abacate', 'Caju', 'Cacau', 'Melancia', 'Limao', 'Goiaba', 'Graviola', 'Uva']
+    palavra = ['banana', 'abacaxi', 'melao', 'maça', 'morango', 'laranja', 'mamao', 
+               'abacate', 'caju', 'cacau', 'melancia', 'limao', 'goiaba', 'graviola', 'uva']
 elif escolha == 2:
     tema = 'Animal'
-    palavra = ['Cachorro', 'Cavalo', 'Onça', 'Coelho', 'Orangotango', 'Elefante', 
-               'Abelha', 'Aranha', 'Borboleta', 'Baleia', 'Camelo', 'Castor', 'Camundongo', 'Capivara']
+    palavra = ['cachorro', 'cavalo', 'onça', 'coelho', 'orangotango', 'elefante', 
+               'abelha', 'aranha', 'borboleta', 'baleia', 'camelo', 'castor', 'camundongo', 'capivara']
 elif escolha == 3:
     tema = 'Humano'
-    palavra = ['Cerebro', 'Perna', 'Braço', 'Esqueleto', 'Musculo', 'Boca', 
-               'Intestino', 'Medula', 'Pele', 'Bexiga', 'Coraçao', 'Rins', 'Pulmao', 'Coluna']
+    palavra = ['cerebro', 'perna', 'braço', 'esqueleto', 'musculo', 'boca', 
+               'intestino', 'medula', 'pele', 'bexiga', 'coraçao', 'rins', 'pulmao', 'coluna']
 elif escolha == 4:
     tema = 'Casa'
-    palavra = ['Telhado', 'Portao', 'Janela', 'Piso', 'Chuveiro', 'Vaso', 'Panela', 
-               'Frigideira', 'Travesseiro', 'Toalha', 'Mesa', 'Copo', 'Guardanapo', 'Almofada']
-else:
-    print('Opção Inválida! Escolha uma das opções para o tema do jogo.')
-
+    palavra = ['telhado', 'portao', 'janela', 'piso', 'chuveiro', 'vaso', 'panela', 
+               'frigideira', 'travesseiro', 'toalha', 'mesa', 'copo', 'guardanapo', 'almofada']
 escolhida = choice(palavra).upper()
-
 tentativas = 6
 letra_correta = []
 letra_incorreta = []
-
 while tentativas > 0:
     letra = input('Insira uma letra: ').upper().strip()
-
     if len(letra) != 1 or not letra.isalpha():
         print('Insira somente uma letra')
         continue
-
     if letra in letra_correta or letra in letra_incorreta:
         print('Você já tentou essa letra')
         continue
-
     if letra in escolhida:
         letra_correta.append(letra)
     else:
         letra_incorreta.append(letra)
         tentativas -= 1
-
     palavra_descoberta = ''
     for letra_palavra in escolhida:
         if letra_palavra in letra_correta:
             palavra_descoberta += letra_palavra
         else:
             palavra_descoberta += '_ '
-
     print('Palavra descoberta: ', palavra_descoberta)
     print('Letras corretas: ', ', '.join(letra_correta))
     print('Letras incorretas: ', ', '.join(letra_incorreta))
     print('Você tem {} tentativa(s) restante(s).'.format(tentativas))
-
     if palavra_descoberta == escolhida:
         print('Parabéns! Você acertou a palavra:', escolhida)
-        jogar_novamente = input('Deseja jogar novamente? (S para Sim ou tecle "ENTER" para sair): ').strip().upper()
-        if jogar_novamente == 'S':
+        jogar_novamente = input('Deseja jogar novamente? ("S" para continuar ou "N" para sair): ').strip().upper()
+        if jogar_novamente == 'N' or jogar_novamente == 'n':
+            print('\nObrigado por jogar! Até mais.\n')
+            break
+        if jogar_novamente == 'S' or jogar_novamente == 's':
             letra_correta = []
             letra_incorreta = []
             escolhida = choice(palavra).upper()
             tentativas = 6
             limpar_tela()
-        else:
-            break
-
 if tentativas == 0:
     print('GAME OVER! A palavra era:', escolhida)
